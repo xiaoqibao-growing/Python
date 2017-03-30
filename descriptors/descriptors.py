790d45a60804e046dd2981d0d4ee12db77682b33
NeuObito <xj174850@163.com>
2017-03-30 18:33:47 +0800

    descriptors

---
+++ descriptors/descriptors.py
@@ -0,0 +1,21 @@
+#-*- coding=utf-8 -*-
+
+class RevealAccess(object):
+	def __get__(self, obj, objtype):
+		print("self in RevealAccess:{}".format(self))
+		print("self:{}\nobjtype:{}".format(self, obj, objtype))
+
+
+class MyClass(object):
+	x = RevealAccess()
+
+	def test(self):
+		print("self in MyClass:{}".format(self))
+
+
+if __name__ == '__main__':
+	m = MyClass()
+	m.test()
+
+	m.x
+	MyClass.x
