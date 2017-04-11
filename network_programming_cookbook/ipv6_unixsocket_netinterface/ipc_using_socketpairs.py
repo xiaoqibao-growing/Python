e810198eeb71c5e07e31bb13ed3b24cb9a888006
NeuObito <xj174850@163.com>
2017-04-11 14:39:03 +0800

    add ipc_socketpairs.py

---
+++ network_programming_cookbook/ipv6_unixsocket_netinterface/ipc_using_socketpairs.py
@@ -0,0 +1,33 @@
+# -*- coding=utf-8 -*-
+
+import socket
+import os
+
+BUFSIZE = 1024
+
+def test_socketpair():
+    """ Test Unix socketpair """
+    parent, child = socket.socketpair()
+    pid = os.fork()
+
+    try:
+        if pid:
+            print("@Parent, sending message...")
+            child.close()
+            parent.sendall("Hello form parent!")
+            response = parent.recv(BUFSIZE)
+            print("Response from child:", response)
+            parent.close()
+        else:
+            print("@Child, waiting for message from parent...")
+            parent.close()
+            message = child.recv(BUFSIZE)
+            print("Message from parent:", message)
+            child.sendall("Hello from child!!")
+            child.close()
+    except Exception as e:
+        print("Error %s" % e)
+
+
+if __name__ == '__main__':
+    test_socketpair()
