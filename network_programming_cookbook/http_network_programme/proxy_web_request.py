1ffe72732de3375c8a4beca0f5437b22e1418d57
NeuObito <xj174850@163.com>
2017-04-12 15:35:49 +0800

    add proxy_web_request.py

---
+++ network_programming_cookbook/http_network_programme/proxy_web_request.py
@@ -0,0 +1,11 @@
+# -*- coding=utf-8 -*-
+
+import urllib
+
+URL = 'https://www.github.com'
+PROXY_ADDRESS = "165.24.10.8:8080"  # By Googling free proxy server
+
+
+if __name__ == '__main__':
+    resp = urllib.urlopen(URL, proxies={'http': PROXY_ADDRESS})
+    print("Proxy server returns response headers: %s" % resp.headers)
